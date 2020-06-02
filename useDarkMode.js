7d6d40a2c4c2de2cf3d5327554ebe3a5856fa208
Dave Schumaker <daveschumaker@users.noreply.github.com>
2020-06-02 09:26:31 -0700

    Update darkMode hook to support Safari.
    It looks like Safari doesn't support `addEventListener` for `window.matchMedia`. Using `addListener` works instead. It looks like it's a bug with Webkit and a patch has been submitted. But as of this PR, it hasn't been merged / deployed to the public yet.
    
    * https://stackoverflow.com/a/60000747
    * https://developer.mozilla.org/en-US/docs/Web/API/MediaQueryList/addListener
    
    Fixes: #1

--- useDarkMode.js
+++ useDarkMode.js
@@ -1,50 +1,56 @@
 import { useState, useEffect } from 'react';
 
 const useDarkMode = () => {
-  const isClient = typeof window === 'object';
+    const isClient = typeof window === 'object';
 
-  const getDarkMode = () => {
-    if (!isClient) {
-      return;
-    }
+    const getDarkMode = () => {
+        if (localStorage.getItem('theme') === 'dark') {
+            return true;
+        } else if (localStorage.getItem('theme') === 'light') {
+            return false;
+        }
 
-    if (localStorage.getItem('theme') === 'dark') {
-      return true;
-    } else if (localStorage.getItem('theme') === 'light') {
-      return false;
-    }
+        if (
+            isClient &&
+            window.matchMedia &&
+            window.matchMedia('(prefers-color-scheme: dark)').matches
+        ) {
+            return true;
+        } else {
+            return false;
+        }
+    };
 
-    if (
-      window.matchMedia &&
-      window.matchMedia('(prefers-color-scheme: dark)').matches
-    ) {
-      return true;
-    } else {
-      return false;
-    }
-  };
+    const [darkMode, setDarkMode] = useState(getDarkMode);
+    const handleChange = () => {
+        setDarkMode(getDarkMode());
+    };
 
-  const [darkMode, setDarkMode] = useState(getDarkMode);
+    useEffect(() => {
+        if (!isClient) {
+            return false;
+        }
 
-  const handleChange = () => {
-    setDarkMode(getDarkMode());
-  };
+        const darkModeQuery = window.matchMedia('(prefers-color-scheme: dark)');
 
-  useEffect(() => {
-    if (!isClient) {
-      return;
-    }
+        try {
+            // Handle Chrome & Firefox
+            darkModeQuery.addEventListener('change', handleChange);
+        } catch (addEventListenerError) {
+            try {
+                // Handle Safari
+                darkModeQuery.addListener('change', handleChange);
+            } catch (addListenerError) {
+                console.error(addListenerError);
+            }
+        }
+        return () =>
+            window
+                .matchMedia('(prefers-color-scheme: dark)')
+                .removeEventListener('change', handleChange);
+    }, []);
 
-    window
-      .matchMedia('(prefers-color-scheme: dark)')
-      .addEventListener('change', handleChange);
-    return () =>
-      window
-        .matchMedia('(prefers-color-scheme: dark)')
-        .removeEventListener('change', handleChange);
-  }, []);
-
-  return darkMode;
+    return darkMode;
 };
 
 export default useDarkMode;
